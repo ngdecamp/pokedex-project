dfc5f417093ed451c55fb8b2d3504942226e5004
ngdecamp <157411293+ngdecamp@users.noreply.github.com>
2024-05-03 11:52:53 -0400

    Pokmon Project Update

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello world');
-let favoriteFood = 'MacNCheese';
-document.write(favoriteFood);
+let pokemonList = [
+    { name: 'Bulbasaur', height: .7, type: ['grass', 'poison']},
+    { type: 'Charmander', height: .6 , type: ['fire']}
+    { type: 'Squirtle', height: .5, type: ['water']}
+  ];
