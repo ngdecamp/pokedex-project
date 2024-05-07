93f84c1c450976303752fabf5377725a36ba07bc
ngdecamp <157411293+ngdecamp@users.noreply.github.com>
2024-05-07 08:31:25 -0400

    Pokedex Commas

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let pokemonList = [
-    { name: 'Bulbasaur' , height: .7 , type: ['grass' , 'poison']} , 
-    { type: 'Charmander' , height: .6 , type: ['fire']} , 
-    { type: 'Squirtle' , height: .5 , type: ['water']} 
+    { name: 'Bulbasaur', height: .7, type: ['grass' , 'poison']}, 
+    { type: 'Charmander', height: .6, type: ['fire']}, 
+    { type: 'Squirtle', height: .5, type: ['water']} 
   ];
