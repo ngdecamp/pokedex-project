e9c7406f3f7b37ebda2f6ae1d41255f41845effb
ngdecamp <157411293+ngdecamp@users.noreply.github.com>
2024-05-11 13:08:39 -0400

    Conditionals and loops

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 let pokemonList = [
     { name: 'Bulbasaur', height: .7, type: ['grass' , 'poison']}, 
-    { type: 'Charmander', height: .6, type: ['fire']}, 
-    { type: 'Squirtle', height: .5, type: ['water']} 
+    { name: 'Charmander', height: .6, type: ['fire']}, 
+    { name: 'Squirtle', height: .5, type: ['water']} 
   ];
+  for (let i=0; i < pokemonList.length; i++){
+    if (pokemonList[i].height >.6){
+      document.write(pokemonList[i].name + " Height: " + pokemonList[i].height + " Wow, that’s big! <br>" );
+    }else {
+      document.write(pokemonList[i].name + " Height: " + pokemonList[i].height + "<br>");
+    }
+  }
