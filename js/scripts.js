6c12b3a76234d3c21a1f0c132c72b70cd638a8ef
ngdecamp <157411293+ngdecamp@users.noreply.github.com>
2024-08-27 18:00:37 -0400

    bad code

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
-let pokemonRepository = (function() {
-  let pokemonList = [
+let pokemonRepository = (function () {
+let pokemonList = [
     { name: 'Bulbasaur', height: .7, type: ['grass' , 'poison']}, 
     { name: 'Charmander', height: .6, type: ['fire']}, 
     { name: 'Squirtle', height: .5, type: ['water']} 
   ];
-  
+
   function add(pokemon) {
     pokemonList.push(pokemon);
   }
@@ -19,10 +19,12 @@ let pokemonRepository = (function() {
   };
 })();
 
-  pokemonRepository.getAll().forEach(pokemon => {
-    if (pokemon.height > .6) {
-      document.write(pokemon.name + " Height: " + pokemon.height + " Wow, that’s big! <br>" );
-    } else {
-      document.write(pokemon.name + " Height: " + pokemon.height + "<br>");
-    }
-  });
+pokemonRepository.forEach(function(getAll) {
+  let element = document.querySelector('.pokemon-list');
+  let listItem = document.createElement('li')
+  let button = document.createElement('button')
+  element.classList.add('pokemonbutton');
+  listItem.appendChild(button)
+  ul.appendChild(listItem)
+}
+  })
