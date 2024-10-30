5ed066b961030f093f4f355e8b9399169a5d82be
ngdecamp <157411293+ngdecamp@users.noreply.github.com>
2024-10-30 11:45:08 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 let pokemonRepository = (function () {
-let pokemonList = [
-    { name: 'Bulbasaur', height: .7, type: ['grass' , 'poison']}, 
-    { name: 'Charmander', height: .6, type: ['fire']}, 
-    { name: 'Squirtle', height: .5, type: ['water']} 
+  let pokemonList = [
+    { name: 'Bulbasaur', height: .7, type: ['grass', 'poison'] },
+    { name: 'Charmander', height: .6, type: ['fire'] },
+    { name: 'Squirtle', height: .5, type: ['water'] }
   ];
 
   function add(pokemon) {
@@ -13,18 +13,29 @@ let pokemonList = [
     return pokemonList;
   }
 
+  function addListItem(pokemon) {
+    let element = document.querySelector('.pokemon-list');
+    let listItem = document.createElement('li');
+    let button = document.createElement('button');
+    button.classList.add('pokemonbutton');
+    button.innerText = pokemon.name; // Add the pokemon name to the button
+    listItem.appendChild(button);
+    element.appendChild(listItem);
+  }
+  
   return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem
   };
 })();
 
-pokemonRepository.forEach(function(getAll) {
+pokemonRepository.getAll().forEach(function (pokemon) {
   let element = document.querySelector('.pokemon-list');
-  let listItem = document.createElement('li')
-  let button = document.createElement('button')
-  element.classList.add('pokemonbutton');
-  listItem.appendChild(button)
-  ul.appendChild(listItem)
-}
-  })
+  let listItem = document.createElement('li');
+  let button = document.createElement('button');
+  button.classList.add('pokemonbutton');
+  button.innerText = pokemon.name; // Add the pokemon name to the button
+  listItem.appendChild(button);
+  element.appendChild(listItem);
+});
